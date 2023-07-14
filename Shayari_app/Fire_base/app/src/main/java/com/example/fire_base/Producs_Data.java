1b4b4f6a8f1851b66591ca7cb031758d80a8c7d5
mayurahir2266 <mayurahir2266@gmail.com>
2023-07-14 12:25:15 +0530

    6

---
+++ Shayari_app/Fire_base/app/src/main/java/com/example/fire_base/Producs_Data.java
@@ -0,0 +1,54 @@
+package com.example.fire_base;
+
+public class Producs_Data
+{
+    String id;
+    String name;
+    String price;
+    String category;
+
+    public Producs_Data(String id, String name, String price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Producs_Data()
+    {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
+
--- Shayari_app/Fire_base/app/src/main/java/com/example/fire_base/ProfileActivity.java
+++ Shayari_app/Fire_base/app/src/main/java/com/example/fire_base/ProfileActivity.java
@@ -3,12 +3,42 @@ package com.example.fire_base;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    TextView submit;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        submit=findViewById(R.id.submitbutton);
+
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                database = FirebaseDatabase.getInstance();
+                myRef = database.getReference("OtherData").push();
+
+                Log.d("TTT", "ref: "+myRef);
+                String id = myRef.getKey();
+                Log.d("TTT", "ID=: "+id);
+
+                Producs_Data producs_data=new Producs_Data(id,"Ravi","123","Developer");
+                myRef.setValue(producs_data);
+            }
+        });
     }
 }
